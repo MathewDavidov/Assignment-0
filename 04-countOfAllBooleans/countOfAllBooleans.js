6e66dd36654c9e34000e08561d7efc748fb888ef
MathewDavidov <41017026+MathewDavidov@users.noreply.github.com>
2020-05-24 21:11:12 -0400

    Add solution to 04

--- 04-countOfAllBooleans/countOfAllBooleans.js
+++ 04-countOfAllBooleans/countOfAllBooleans.js
@@ -1,5 +1,15 @@
 function countOfAllBooleans(arr) {
-  // Insert code here;
+  // Count the number of booleans in the array
+  var numOfBooleans = 0;
+
+  // Loop through the array and check the type of each object
+  for (var i=0; i<arr.length; i++) {
+    if (arr[i] === true || arr[i] === false) {
+      numOfBooleans++;
+    }
+  }
+
+  return numOfBooleans;
 }
 
 // Do not edit this line;
