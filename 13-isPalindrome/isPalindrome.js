9ab19d12f94f109449272804aa134046d1924857
MathewDavidov <41017026+MathewDavidov@users.noreply.github.com>
2020-05-25 01:12:15 -0400

    Add solution to 13

--- 13-isPalindrome/isPalindrome.js
+++ 13-isPalindrome/isPalindrome.js
@@ -1,5 +1,15 @@
 function isPalindrome(word) {
-  // Insert code here;
+  // Recursively determine if the string is a palindrome
+
+  if (word.length === 0 || word.length === 1) {
+    return true;
+  } 
+
+  if (word.charAt(0) === word.charAt(word.length-1)) {
+    return isPalindrome(word.substring(1, word.length - 1));
+  }
+
+  return false;
 }
 
 // Do not edit this line;
