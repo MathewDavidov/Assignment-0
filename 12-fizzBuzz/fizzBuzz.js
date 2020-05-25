2cce365593abd524945f9a9db8fe2012f9134613
MathewDavidov <41017026+MathewDavidov@users.noreply.github.com>
2020-05-25 01:01:52 -0400

    Add solution to 12

--- 12-fizzBuzz/fizzBuzz.js
+++ 12-fizzBuzz/fizzBuzz.js
@@ -1,5 +1,25 @@
 function fizzBuzz(start, end) {
-  // Insert code here;
+  // Given a range start -> end
+  // If a number is divisible by 3 and 5, insert "FizzBuzz"
+  // If a number is divisble by 5, insert "Buzz"
+  // If a number is divisble by 3, insert "Fizz"
+  // Otherwise, enter the number
+
+  let arr = [];
+
+  for (let i=start; i<=end; i++) {
+    if (i % 3 === 0 && i % 5 === 0) {
+      arr.push("FizzBuzz");
+    } else if (i % 5 === 0) {
+      arr.push("Buzz");
+    } else if (i % 3 === 0) {
+      arr.push("Fizz");
+    } else {
+      arr.push(i);
+    }
+  }
+
+  return arr;
 }
 
 // Do not edit this line;
