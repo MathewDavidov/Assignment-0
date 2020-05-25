e0c320c0734c67819dff51bafa1b633e2c99d9e1
MathewDavidov <41017026+MathewDavidov@users.noreply.github.com>
2020-05-25 15:15:52 -0400

    Change logic to make sure two distinct indices create a pair

--- 18-pairSum/pairSum.js
+++ 18-pairSum/pairSum.js
@@ -8,7 +8,7 @@ function pairSum(nums, target) {
 
   for (let i=0; i<nums.length; i++) {
     for (let j=i+1; j<nums.length; j++) {
-      if (nums[i] + nums[j] === target) {
+      if (i !== j && (nums[i] + nums[j] === target)) {
         return true;
       }
     }
