5adf0988f36e7f4f6a1f4d32febf9452cd5ebcad
MathewDavidov <41017026+MathewDavidov@users.noreply.github.com>
2020-05-25 00:17:03 -0400

    Add solution to 05

--- 05-countOfAllBooleansAndStrings/countOfAllBooleansAndStrings.js
+++ 05-countOfAllBooleansAndStrings/countOfAllBooleansAndStrings.js
@@ -1,5 +1,15 @@
 function countOfAllBooleansAndStrings(arr) {
-  // Insert code here;
+  // Count the number of booleans and strings in the array
+  var numOfBooleansAndStrings = 0;
+
+  // Loop through the array and check the type of each object
+  for (var i = 0; i < arr.length; i++) {
+    if (arr[i] === true || arr[i] === false || typeof arr[i] === 'string') {
+      numOfBooleansAndStrings++;
+    }
+  }
+
+  return numOfBooleansAndStrings;
 }
 
 // Do not edit this line;
