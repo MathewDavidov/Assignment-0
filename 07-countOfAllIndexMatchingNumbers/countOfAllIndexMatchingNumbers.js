09905f4b696734e18287f6b24f2ba014767888ed
MathewDavidov <41017026+MathewDavidov@users.noreply.github.com>
2020-05-25 00:30:12 -0400

    Add solution to 07

--- 07-countOfAllIndexMatchingNumbers/countOfAllIndexMatchingNumbers.js
+++ 07-countOfAllIndexMatchingNumbers/countOfAllIndexMatchingNumbers.js
@@ -1,5 +1,15 @@
 function countOfAllIndexMatchingNumbers(nums) {
-  // Insert code here;
+  // Count the amount of times the index of an array is the same as the number at that index
+
+  let count = 0;
+
+  for (let i=0; i<nums.length; i++) {
+    if (i === nums[i]) {
+      count++;
+    }
+  }
+
+  return count;
 }
 
 // Do not edit this line;
