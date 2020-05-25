26ff93afb0aa27c768dae7af013cb76ed3bad3cb
Mathew Davidov <41017026+MathewDavidov@users.noreply.github.com>
2020-05-25 00:19:24 -0400

    change var to let

--- 05-countOfAllBooleansAndStrings/countOfAllBooleansAndStrings.js
+++ 05-countOfAllBooleansAndStrings/countOfAllBooleansAndStrings.js
@@ -1,9 +1,9 @@
 function countOfAllBooleansAndStrings(arr) {
   // Count the number of booleans and strings in the array
-  var numOfBooleansAndStrings = 0;
+  let numOfBooleansAndStrings = 0;
 
   // Loop through the array and check the type of each object
-  for (var i = 0; i < arr.length; i++) {
+  for (let i = 0; i < arr.length; i++) {
     if (arr[i] === true || arr[i] === false || typeof arr[i] === 'string') {
       numOfBooleansAndStrings++;
     }
