6d324ddcad76fd63cdd0d96c75c937916b9a9b99
MathewDavidov <41017026+MathewDavidov@users.noreply.github.com>
2020-05-25 00:33:25 -0400

    Add solution to 08

--- 08-sumOfAllEvenNumbers/sumOfAllEvenNumbers.js
+++ 08-sumOfAllEvenNumbers/sumOfAllEvenNumbers.js
@@ -1,5 +1,15 @@
 function sumOfAllEvenNumbers(nums) {
-  // Insert code here;
+  // Count the amount of numbers in the array that are even
+  let count = 0;
+
+  // For each number in the array, check if it's even. If so, increment count.
+  nums.forEach((number) => {
+    if (number % 2 == 0) {
+      count++;
+    }
+  })
+
+  return count;
 }
 
 // Do not edit this line;
