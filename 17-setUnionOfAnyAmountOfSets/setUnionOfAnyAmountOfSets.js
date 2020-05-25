b6547f538f8a0fb40d83e925cce87575f9c3248a
MathewDavidov <41017026+MathewDavidov@users.noreply.github.com>
2020-05-25 01:40:09 -0400

    Add solution to 17

--- 17-setUnionOfAnyAmountOfSets/setUnionOfAnyAmountOfSets.js
+++ 17-setUnionOfAnyAmountOfSets/setUnionOfAnyAmountOfSets.js
@@ -1,5 +1,15 @@
 function setUnionOfAnyAmountOfSets(...args) {
-  // Insert code here;
+  // Create the set union on any given number of sets
+
+  let finalSet = new Set();
+
+  for (const set of args) {
+    for (const number of set) {
+      finalSet.add(number);
+    }
+  }
+
+  return finalSet;
 }
 
 // Do not edit this line;
