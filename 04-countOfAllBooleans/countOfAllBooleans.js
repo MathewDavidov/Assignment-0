b3083dfd160cea341fff8c690622f436912f12b6
Mathew Davidov <41017026+MathewDavidov@users.noreply.github.com>
2020-05-25 00:22:59 -0400

    change var to let

--- 04-countOfAllBooleans/countOfAllBooleans.js
+++ 04-countOfAllBooleans/countOfAllBooleans.js
@@ -1,9 +1,9 @@
 function countOfAllBooleans(arr) {
   // Count the number of booleans in the array
-  var numOfBooleans = 0;
+  let numOfBooleans = 0;
 
   // Loop through the array and check the type of each object
-  for (var i=0; i<arr.length; i++) {
+  for (let i=0; i<arr.length; i++) {
     if (arr[i] === true || arr[i] === false) {
       numOfBooleans++;
     }
