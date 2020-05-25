ed76198307e85bcaf5a75d2120a6affd13bbe62c
MathewDavidov <41017026+MathewDavidov@users.noreply.github.com>
2020-05-25 01:21:18 -0400

    Add solution to 14

--- 14-frequencyCounter/frequencyCounter.js
+++ 14-frequencyCounter/frequencyCounter.js
@@ -1,5 +1,20 @@
 function frequencyCounter(word) {
-  // Insert code here;
+  // Create a mapping of key frequency 
+
+  let map = {};
+
+  // Split the string into an array of letters
+  // If the key exists, increment
+  // Otherwise, add the letter as a key and initialize its value to 1
+  word.split('').forEach((letter) => {
+    if (map[letter]) {
+      map[letter] += 1;
+    } else {
+      map[letter] = 1;
+    }
+  });
+
+  return map;
 }
 
 // Do not edit this line;
