5fd35c92c30203eb4c867b8b118f5925c80608b5
MathewDavidov <41017026+MathewDavidov@users.noreply.github.com>
2020-05-25 01:50:15 -0400

    Add solution to 18

--- 18-pairSum/pairSum.js
+++ 18-pairSum/pairSum.js
@@ -1,5 +1,20 @@
 function pairSum(nums, target) {
-  // Insert code here;
+  // Find if any two numbers in the given array equal the given target
+  // This solution is O(n^2)
+
+  if (nums.length <= 1) {
+    throw "Array is less than length 2";
+  }
+
+  for (let i=0; i<nums.length; i++) {
+    for (let j=i+1; j<nums.length; j++) {
+      if (nums[i] + nums[j] === target) {
+        return true;
+      }
+    }
+  }
+
+  return false;
 }
 
 // Do not edit this line;
