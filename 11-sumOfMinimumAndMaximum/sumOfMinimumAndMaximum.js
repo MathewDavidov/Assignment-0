6d9ab7862860c1399a599bf50555d4c69b83c0d6
MathewDavidov <41017026+MathewDavidov@users.noreply.github.com>
2020-05-25 00:48:54 -0400

    Add solution to 11

--- 11-sumOfMinimumAndMaximum/sumOfMinimumAndMaximum.js
+++ 11-sumOfMinimumAndMaximum/sumOfMinimumAndMaximum.js
@@ -1,5 +1,19 @@
 function sumOfMinimumAndMaximum(nums) {
-  // Insert code here;
+  // Find the sum of the minimum and maximum in the given array
+
+  let min = nums[0];
+  let max = nums[0];
+
+  nums.forEach((number) => {
+    if (number > max) {
+      max = number;
+    }
+    if (number < min) {
+      min = number;
+    }
+  });
+
+  return max+min;
 }
 
 // Do not edit this line;
