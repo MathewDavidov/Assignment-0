d34be53e0fec29f30f7b4308c9da3d41bb090414
MathewDavidov <41017026+MathewDavidov@users.noreply.github.com>
2020-05-25 01:31:31 -0400

    Add solution to 16

--- 16-productOfAnyAmountOfNumbers/productOfAnyAmountOfNumbers.js
+++ 16-productOfAnyAmountOfNumbers/productOfAnyAmountOfNumbers.js
@@ -1,5 +1,13 @@
 function productOfAnyAmountOfNumbers(...args) {
-  // Insert code here;
+  // Return the product of a given number of arguments
+
+  let product = 1;
+
+  args.forEach((number) => {
+    product *= number;
+  })
+
+  return product;
 }
 
 // Do not edit this line;
